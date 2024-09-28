804ddf970cb78d6f0623b1a699359cb163fbb9ce
PK <pradeepdhital124@gmail.com>
2024-09-28 16:12:30 +0800

    done for purchases page and more-actions functioning for now

--- src/pages/purchases/purchasesPage.js
+++ src/pages/purchases/purchasesPage.js
@@ -3,6 +3,7 @@ import { AppLayOut } from "../../components/layout/AppLayOut";
 import { PurchasesCard } from "../../components/purchases-card/PurchasesCard";
 import { Col, Row } from "react-bootstrap";
 import { useSelector } from "react-redux";
+import "./purchases.css";
 
 const Purchases = () => {
   const { favourites } = useSelector((state) => state.system);
@@ -13,7 +14,7 @@ const Purchases = () => {
         <Row>
           {favourites.map((item, i) => {
             return (
-              <Col lg={6} md={6} sm={12}>
+              <Col lg={6} md={6} sm={12} className="purchases-column">
                 <PurchasesCard></PurchasesCard>
               </Col>
             );
