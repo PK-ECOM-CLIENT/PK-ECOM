07cf080cb4c67ec2179247654d935d83108c4770
Pradeep Kumar Dhital <pradeepdhital124@gmail.com>
2025-08-02 14:13:42 +0800

    working on search icon

---
+++ src/components/layout-components/originalHeader.js
@@ -0,0 +1,126 @@
+<Navbar expand="lg" variant="dark" className="header_navbar color-white">
+      <div className="container-fluid">
+        <Col md={2}>
+          <Navbar.Brand className="navbar-brand d-flex flex-column justify-content-center align-items-center">
+            <img src={logo} alt="logo" className="logo" />
+          </Navbar.Brand>
+        </Col>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="navbar-nav ms-auto mb-2 mb-lg-0">
+            {windowWidth < 992 && (
+              <div className="dropdown-wrapper" style={{ position: "relative" }}>
+                <div className="color-white search__filter-toggle" onClick={handleCategoriesDropdownToggle} style={{ cursor: "pointer" }}>
+                  Categories
+                  <i className={`fa-solid fa-caret-down ${isCategoriesDropdownOpen ? "-util-rotate_180" : ""}`} style={{ marginLeft: "5px" }}></i>
+                </div>
+                <Dropdown show={isCategoriesDropdownOpen}>
+                  <Dropdown.Menu variant="light" className="user_profile__profie-dropdown-items" style={{ position: "absolute", top: "100%", left: 0 }}>
+                    {categories.map(
+                      (item, i) =>
+                        !item.catId && (
+                          <Link className="nav-link dropdown-item" to={`/categories/${item._id}`} key={i}>
+                            {item.name === "Home & Kitchen" ? (
+                              <span>{item.name}<span className="-util-nav">*</span></span>
+                            ) : (
+                              item.name
+                            )}
+                          </Link>
+                        )
+                    )}
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            )}
+
+            {url !== "/" && <li className="nav-item"><Link className="nav-link active" to="/">Home</Link></li>}
+            <li className="nav-item"><Link className={`nav-link active ${url.includes("/offers") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/offers">Offers</Link></li>
+            <li className="nav-item"><Link className={`nav-link active ${url.includes("/bestsellers") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/bestsellers">Best Sellers</Link></li>
+            <li className="nav-item"><Link className={`nav-link active ${url.includes("/newarrivals") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/newarrivals">New Arrivals</Link></li>
+            <li className="nav-item"><Link className={`nav-link active ${url.includes("/dealsandsales") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/dealsandsales">Deals and Sales</Link></li>
+
+            <li className="nav-item nav_icons">
+              <Link className={`nav-link active nav_icons__icon ${url.includes("/favourites") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/favourites">
+                {windowWidth > 991 ? <i className="fa-solid fa-heart -util-font15"></i> : "Favourites"}
+              </Link>
+              <span className="nav_icons__count">{favourites?.length}</span>
+            </li>
+
+            <li className="nav-item nav_icons">
+              <Link className={`nav-link active nav_icons__icon ${url.includes("/cart") && windowWidth > 991 ? "-util-underline" : "-util-underline-transparent"}`} to="/cart">
+                {windowWidth > 991 ? <i className="fa-solid fa-cart-shopping -util-font15"></i> : "Cart"}
+              </Link>
+              <span className="nav_icons__count">{cart?.length}</span>
+            </li>
+
+            {windowWidth < 992 && user?._id ? (
+              <div className="dropdown-wrapper" style={{ position: "relative" }}>
+                <div className="profile-toggle text-white" onClick={handleDropdownToggle} style={{ cursor: "pointer" }}>
+                  Profile
+                  <i className={`fa-solid fa-caret-down ${isDropdownOpen ? "-util-rotate_180" : ""}`} style={{ marginLeft: "5px" }}></i>
+                </div>
+                <Dropdown show={isDropdownOpen}>
+                  <Dropdown.Menu className="user_profile__profie-dropdown-items" style={{ position: "absolute", top: "100%", right: 0 }}>
+                    <Link className="nav-link dropdown-item">Profile</Link>
+                    <Link className="nav-link dropdown-item" to="/purchases">Purchases</Link>
+                    <Link className="nav-link dropdown-item">Reviews</Link>
+                    <Link className="nav-link dropdown-item">Payment Methods</Link>
+                    <Link className="nav-link dropdown-item">Close Account</Link>
+                    <Link className="nav-link dropdown-item">Switch Account</Link>
+                    <Link className="nav-link dropdown-item" onClick={handleOnLogout}>Sign Out</Link>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+            ) : (
+              windowWidth < 992 && (
+                <div className="-util-pointer" onClick={handleOnLogin}>Login</div>
+              )
+            )}
+          </Nav>
+
+          <div className="search-and-icons">
+            <Form className="d-flex align-items-center mb-2 categories_search" role="search">
+              <Form.Control className="form-control me-2 search_form__control" type="text" aria-label="Search" placeholder="Search" />
+              <Button variant="none" className="search_icon">
+                <i className="fa-solid fa-magnifying-glass -util-font15 color-pastrelred"></i>
+              </Button>
+            </Form>
+          </div>
+
+          <div className="user color-white">
+            {windowWidth > 991 && (
+              <div className="user_profile">
+                <p className="user_profile__user user_profile__address">
+                  {user?._id
+                    ? `Hello ${user.firstName} ! Delivery Address: ${address}`
+                    : `Dear valued client! Wishing you a great ${
+                        hour < 4 ? "night" : hour < 12 ? "morning" : hour < 17 ? "day" : hour < 21 ? "evening" : "night"
+                      }!`}
+                </p>
+                {user?._id ? (
+                  <p className="user_profile__profile" onClick={handleDropdownToggle} style={{ cursor: "pointer" }}>
+                    <div className="user_profile__profie-btn btn-logout text-white d-flex align-items-center">
+                      <i className="fa-solid fa-user -util-font15"></i>
+                      <i className={`fa-solid fa-caret-down ${isDropdownOpen ? "-util-rotate_180" : ""}`}></i>
+                    </div>
+                    <Dropdown show={isDropdownOpen}>
+                      <Dropdown.Menu variant="light" className="user_profile__profie-dropdown-items">
+                        <Link className="nav-link dropdown-item">Profile</Link>
+                        <Link className="nav-link dropdown-item" to="/purchases">Purchases</Link>
+                        <Link className="nav-link dropdown-item">Reviews</Link>
+                        <Link className="nav-link dropdown-item">Payment Methods</Link>
+                        <Link className="nav-link dropdown-item">Close Account</Link>
+                        <Link className="nav-link dropdown-item">Switch Account</Link>
+                        <Link className="nav-link dropdown-item" onClick={handleOnLogout}>Sign Out</Link>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </p>
+                ) : (
+                  <div className="nav-link header_login-btn" onClick={handleOnLogin}>Login</div>
+                )}
+              </div>
+            )}
+          </div>
+        </Navbar.Collapse>
+      </div>
+    </Navbar>
--- src/components/navbarsvariant/Bigscreens.js
+++ src/components/navbarsvariant/Bigscreens.js
@@ -72,223 +72,116 @@ const Bigscreens = () => {
   const hour = new Date().getHours();
 
   return (
-    // <Navbar expand="sm" variant="dark" className="header_navbar color-white">
-    //   <div className="container-fluid">
-    //     <Col md={2}>
-    //       <Navbar.Brand className="navbar-brand d-flex flex-column justify-content-center align-items-center">
-    //         <img src={logo} alt="logo" className="logo" />
-    //       </Navbar.Brand>
-    //     </Col>
-    //     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    //     <Navbar.Collapse id="basic-navbar-nav">
-    //       <Nav className="navbar-nav">
-    //         <div className="navdiv">
-    //           <div className="user_profile">
-    //             <div className="user_profile__user user_profile__address">
-    //               {user?._id
-    //                 ? `Hello ${user.firstName} ! Delivery Address: ${address}`
-    //                 : `Dear valued client! Wishing you a great ${
-    //                     hour < 4
-    //                       ? "night"
-    //                       : hour < 12
-    //                       ? "morning"
-    //                       : hour < 17
-    //                       ? "day"
-    //                       : hour < 21
-    //                       ? "evening"
-    //                       : "night"
-    //                   }!`}
-    //             </div>
-    //             <div className="user_profile_login">
-    //               {user?._id ? (
-    //                 <p
-    //                   className="user_profile__profile"
-    //                   onClick={handleDropdownToggle}
-    //                   style={{ cursor: "pointer" }}
-    //                 >
-    //                   <div className="user_profile__profie-btn btn-logout text-white d-flex align-items-center">
-    //                     <i className="fa-solid fa-user -util-font15"></i>
-    //                     <i
-    //                       className={`fa-solid fa-caret-down ${
-    //                         isDropdownOpen ? "-util-rotate_180" : ""
-    //                       }`}
-    //                     ></i>
-    //                   </div>
-    //                   <Dropdown show={isDropdownOpen}>
-    //                     <Dropdown.Menu
-    //                       variant="light"
-    //                       className="user_profile__profie-dropdown-items"
-    //                     >
-    //                       <Link className="nav-link dropdown-item">
-    //                         Profile
-    //                       </Link>
-    //                       <Link
-    //                         className="nav-link dropdown-item"
-    //                         to="/purchases"
-    //                       >
-    //                         Purchases
-    //                       </Link>
-    //                       <Link className="nav-link dropdown-item">
-    //                         Reviews
-    //                       </Link>
-    //                       <Link className="nav-link dropdown-item">
-    //                         Payment Methods
-    //                       </Link>
-    //                       <Link className="nav-link dropdown-item">
-    //                         Close Account
-    //                       </Link>
-    //                       <Link className="nav-link dropdown-item">
-    //                         Switch Account
-    //                       </Link>
-    //                       <Link
-    //                         className="nav-link dropdown-item"
-    //                         onClick={handleOnLogout}
-    //                       >
-    //                         Sign Out
-    //                       </Link>
-    //                     </Dropdown.Menu>
-    //                   </Dropdown>
-    //                 </p>
-    //               ) : (
-    //                 <div
-    //                   className="nav-link header_login-btn"
-    //                   onClick={handleOnLogin}
-    //                 >
-    //                   Login
-    //                 </div>
-    //               )}
-    //             </div>
-    //           </div>
-    //           <div className="search-and-icons">
-    //             <Form
-    //               className="d-flex align-items-center mb-2 categories_search"
-    //               role="search"
-    //             >
-    //               <Form.Control
-    //                 className="form-control me-2 search_form__control"
-    //                 type="text"
-    //                 aria-label="Search"
-    //                 placeholder="Search"
-    //               />
-    //               <Button variant="none" className="search_icon">
-    //                 <i className="fa-solid fa-magnifying-glass -util-font15 color-pastrelred"></i>
-    //               </Button>
-    //             </Form>
-    //           </div>
-    //           <div className="nav-link">
-    //             {url !== "/" && (
-    //               <li className="nav-item">
-    //                 <Link className="nav-link active" to="/">
-    //                   Home
-    //                 </Link>
-    //               </li>
-    //             )}
-    //             <li className="nav-item">
-    //               <Link
-    //                 className={`nav-link active ${
-    //                   url.includes("/offers")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/offers"
-    //               >
-    //                 Offers
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link
-    //                 className={`nav-link active ${
-    //                   url.includes("/bestsellers")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/bestsellers"
-    //               >
-    //                 Best Sellers
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link
-    //                 className={`nav-link active ${
-    //                   url.includes("/newarrivals")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/newarrivals"
-    //               >
-    //                 New Arrivals
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item">
-    //               <Link
-    //                 className={`nav-link active ${
-    //                   url.includes("/dealsandsales")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/dealsandsales"
-    //               >
-    //                 Deals and Sales
-    //               </Link>
-    //             </li>
-    //             <li className="nav-item nav_icons">
-    //               <Link
-    //                 className={`nav-link active nav_icons__icon ${
-    //                   url.includes("/favourites")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/favourites"
-    //               >
-    //                 <i className="fa-solid fa-heart -util-font15"></i>
-    //               </Link>
-    //               <span className="nav_icons__count">{favourites?.length}</span>
-    //             </li>
-
-    //             <li className="nav-item nav_icons">
-    //               <Link
-    //                 className={`nav-link active nav_icons__icon ${
-    //                   url.includes("/cart")
-    //                     ? "-util-underline"
-    //                     : "-util-underline-transparent"
-    //                 }`}
-    //                 to="/cart"
-    //               >
-    //                 <i className="fa-solid fa-cart-shopping -util-font15"></i>
-    //               </Link>
-    //               <span className="nav_icons__count">{cart?.length}</span>
-    //             </li>
-    //           </div>
-    //         </div>
-    //       </Nav>
-    //     </Navbar.Collapse>
-    //   </div>
-    // </Navbar>
-    <div className="navdiv">
-      <div className="logo">LOGO</div>
+    <div className="navdiv header_navbar color-white">
+      <Col md={2}>
+        <Navbar.Brand className="navbar-brand d-flex flex-column justify-content-center align-items-center">
+          <img src={logo} alt="logo" className="logo" />
+        </Navbar.Brand>
+      </Col>
       <div className="contents">
         <div className="client_login_search_wrapper">
           <div className="client_login_search">
+            {/* Wrapper for client and login */}
             <div className="client_login">
               <div className="client">
-                Dear ClienFDFSDFSDFSDDFSDFSDFSFSDFDFSDFt
+                {user?._id
+                  ? `Hello ${user.firstName} ! Delivery Address: ${address}`
+                  : `Dear valued client! Wishing you a great ${
+                      hour < 4
+                        ? "night"
+                        : hour < 12
+                        ? "morning"
+                        : hour < 17
+                        ? "day"
+                        : hour < 21
+                        ? "evening"
+                        : "night"
+                    }!`}
               </div>
-              <div className="login">Login</div>
+              <div>
+                {user?._id ? (
+                  <p
+                    onClick={handleDropdownToggle}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div>
+                      <i className="fa-solid fa-user -util-font15"></i>
+                      <i
+                        className={`fa-solid fa-caret-down ${
+                          isDropdownOpen ? "-util-rotate_180" : ""
+                        }`}
+                      ></i>
+                    </div>
+                    <Dropdown show={isDropdownOpen}>
+                      <Dropdown.Menu
+                        variant="light"
+                        className="user_profile__profie-dropdown-items"
+                      >
+                        <Link className="nav-link dropdown-item">Profile</Link>
+                        <Link
+                          className="nav-link dropdown-item"
+                          to="/purchases"
+                        >
+                          Purchases
+                        </Link>
+                        <Link className="nav-link dropdown-item">Reviews</Link>
+                        <Link className="nav-link dropdown-item">
+                          Payment Methods
+                        </Link>
+                        <Link className="nav-link dropdown-item">
+                          Close Account
+                        </Link>
+                        <Link className="nav-link dropdown-item">
+                          Switch Account
+                        </Link>
+                        <Link
+                          className="nav-link dropdown-item"
+                          onClick={handleOnLogout}
+                        >
+                          Sign Out
+                        </Link>
+                      </Dropdown.Menu>
+                    </Dropdown>
+                  </p>
+                ) : (
+                  <div
+                    className="nav-link header_login-btn -util-link"
+                    onClick={handleOnLogin}
+                  >
+                    Login
+                  </div>
+                )}
+              </div>
             </div>
-            <input className="search" placeholder="Search..." />
+
+            {/* Search form that matches width of client_login */}
+            <div className="search_form_wrapper">
+              <Form className="search_form" role="search">
+                <Form.Control
+                  className="form-control search_form__control"
+                  type="text"
+                  aria-label="Search"
+                  placeholder="Search"
+                />
+                <Button variant="none" className="search_icon">
+                  <i className="fa-solid fa-magnifying-glass -util-font15 color-pastrelred"></i>
+                </Button>
+              </Form>
+            </div>
           </div>
         </div>
-        <div className="content_options">
-          <div className="options">Categories</div>
-          <div className="options">Offers</div>
-          <div className="options">New Arrivals</div>
-          <div className="options">Favourites</div>
-          <div className="options">Cart</div>{" "}
-          <div className="options">Offers</div>
-          <div className="options">New Arrivals</div>
-          <div className="options">Favourites</div>
-          <div className="options">Cart</div>
-        </div>
+
+        <ul className="content_options">
+          <li>dfsdf</li>
+          <li>Categories</li>
+          <li>Offers</li>
+          <li>New Arrivals</li>
+          <li>Favourites</li>
+          <li>Cart</li> <li>Offers</li>
+          <li>New Arrivals</li>
+          <li>Favourites</li>
+          <li>Cart</li>
+        </ul>
       </div>
     </div>
   );
